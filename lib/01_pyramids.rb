4f7224d7498d77a1e21a71374922cb443495af0d
LucasJego <jego.lucas@gmail.com>
2019-10-03 11:40:07 +0200

    Ajout d'une première version de 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -19,16 +19,45 @@ def full_pyramid
 	number_floors.times do |i|
 		puts " "*(number_floors-1-i) + "#"*j
 		j += 2
-
-
-	end
-
-
-
-
+	end 
 end
 
 
 
-full_pyramid
+def wtf_pyramid
+	print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (nombre d'étage impair)\n> "
+	number_floors = gets.to_i
+
+	while number_floors % 2 == 0 do
+		print "Veuillez saisir un nombre impair. Combien d'étages veux-tu ?\n> "
+		number_floors = gets.to_i
+	end
+
+	puts "Voici la pyramide : "
+
+	# Partie supérieure de la pyramide
+	number_dieses = 1
+	number_floors_sens_1 = (number_floors/2 +1)
+	number_spaces = number_floors_sens_1 - 1
+	number_floors_sens_1.times do |i|
+		puts " "*(number_spaces) + "#"*number_dieses
+		number_dieses += 2
+		number_spaces -= 1
+	end
+ 
+ 	# Partie inférieure de la pyramide
+	number_floors_sens_2 = (number_floors/2)
+	number_spaces = 1
+	number_dieses = number_floors-2
+	number_floors_sens_2.times do |i|
+		puts " "*(number_spaces) + "#"*number_dieses
+		number_dieses -= 2
+		number_spaces +=1
+
+	end
+end
+
+
+#full_pyramid
+wtf_pyramid
 
