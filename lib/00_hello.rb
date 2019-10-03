df137d6741415fcbe632afd2f9fb3d8fb5d90673
LucasJego <jego.lucas@gmail.com>
2019-10-03 09:55:53 +0200

    Ajout de l'exo 00_hello terminé

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,3 +1,10 @@
+# Fonction qui prend en valeur la variable first_name et qui affiche "Bonjour + first_name"
+def say_hello(first_name)
+	puts "Bonjour, #{first_name} !"
+end
+
+
+# Fonction qui demande le nom de l'utilisateur et et qui renvoie ce nom
 def ask_first_name
 	puts "Quel est votre nom?"
 	first_name = gets.chomp
@@ -6,12 +13,12 @@ end
 
 
 
+# Fonction qui met dans une variable first_name le résultat de la fonction ask_first_name et qui appelle la fonction say_hello avec comme variable le résultat de la fonction ask_first_name
+def perform
 
-def say_hello(first_name)
-	puts "Bonjour, #{first_name} !"
-
-
+	first_name = ask_first_name
+	say_hello(first_name)
 end
 
-ask_first_name
-say_hello("Dude")
+
+perform
