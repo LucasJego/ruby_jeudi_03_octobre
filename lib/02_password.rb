31f03630bee0c043ca07cb4dbf53c873e9673b23
LucasJego <jego.lucas@gmail.com>
2019-10-03 22:26:39 +0200

    Ajout de l'exo 02 terminé.

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,29 +1,25 @@
 def signup
 	puts "Veuillez saisir un nouveau mot de passe : "
-	new_password = gets.chomp
-	return new_password
+	$new_password = gets.chomp
+	return $new_password # Le symbole $ permet de définir une variable globale qui pourra être utilisée en dehors de la méthode signup
 end
 
-
-
-new_password = signup
-
-def login(new_password)
+ 
+def login
 	puts "Veuillez rentrez votre mot de passe : "
 	password = gets.chomp
 
 	# Tant que le mot de passe rentré n'est pas identique au mot de passe initial contenu dans la variable new_password
-	while password != new_password do
+	while password != $new_password do
 		# Afficher que le mot de passe n'est pas le bon et redemander la saisie
 		puts "Le mot de passe n'est pas reconnu."
 		puts "Veuillez rentrez votre mot de passe : "
 		password = gets.chomp
 	end
 
-	puts "Ok"
+	puts "Mot de passe correct."
 
 end
-
 
 
 def welcome_screen
@@ -31,32 +27,11 @@ def welcome_screen
 end
 
 
-#login(new_password)
-#signup
-#login(new_password)
 
 def perform
-	sign = signup
-	log = login(new_password)
-	welcome
-
-
+	signup
+	login
+	welcome_screen
 end
 
-#perform
-
-
-
-
-
-#-----------------------------------------------------------------------------
-
-# En utilisant une variable globale
-
-def signup
-	puts "Veuillez saisir un nouveau mot de passe : "
-	$new_password = gets.chomp
-	return $new_password
-end
-
-signup
+perform
